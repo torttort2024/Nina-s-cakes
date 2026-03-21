bf835d2b04e6dc2464b3864e697e273befe64634
torttort2024 <tort.nina2024@gmail.com>
2026-03-21 15:10:03 +0200

    Add files via upload

--- js/poisk.js
+++ js/poisk.js
@@ -1,5 +1,5 @@
 function poiskProdukta() {
-    const array = ["молоко", "кефир", "ряженка", "йогурт", "сметана", "творог", "брынза", "сыр", "масло"];
+    const array = ["Торт", "Пирожное", "Кекс", "Трубочка", "Еклеры"];
     const even = (element) => element === document.getElementById("poisk").value.toLowerCase();
     //если не находит товар из списка то выводит сообщение для пользователя
     if(!array.some(even)){
